c20ae32a7bc1d5666a68ca6be216702c7c0d5891
Bonharrt <107985722+Bonharrt@users.noreply.github.com>
2022-07-08 15:45:00 +0200

    Update install_3cx.sh

--- install_3cx.sh
+++ install_3cx.sh
@@ -2,9 +2,7 @@
 
 apt-get update
 apt-get upgrade
-echo "" > /etc/defaut/locale
-echo "LANG=fr_FR.UTF-8" > /etc/defaut/locale
-locale-gen
+export LANG=es_ES.UTF-8
 apt install -y net-tools dphys-swapfile
 apt install gnupg2 -y
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
