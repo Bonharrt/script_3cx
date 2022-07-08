27ac9f8bbd05a684f6fc6e295f573517267ba434
Bonharrt <107985722+Bonharrt@users.noreply.github.com>
2022-07-08 15:02:27 +0200

    Add files via upload

---
+++ install_3cx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo passwd
+su
+apt-get update
+apt-get upgrade
+echo "" > /etc/defaut/locale
+echo "LANG=fr_FR.UTF-8" > /etc/defaut/locale
+locale-gen
+apt install -y net-tools dphys-swapfile
+apt install gnupg2 -y
+wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
+echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | tee /etc/apt/sources.list.d/3cxpbx.list
+apt-get install 3cxpbx
