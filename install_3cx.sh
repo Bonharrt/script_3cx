32d846deceaaf7cfd772aad19a1a25ded1e4b135
Bonharrt <107985722+Bonharrt@users.noreply.github.com>
2022-07-08 15:59:03 +0200

    Update install_3cx.sh

--- install_3cx.sh
+++ install_3cx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
 export LANG=fr_FR.UTF-8
+apt install -y net-tools dphys-swapfile gnupg2
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | tee /etc/apt/sources.list.d/3cxpbx.list
-apt install -y net-tools dphys-swapfile gnupg2 3cxpbx
+apt install -y 3cxpbx
 
